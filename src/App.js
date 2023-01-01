204d18e029de31a606d8f3af942012b52a4b659f
aysunitai <aysun.itai@gmail.com>
2023-01-01 11:21:31 +0200

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Select from "react-select";
 
 function App() {
+  const options=[
+      {value:"january",label:"january"},
+      {value:"Feburary",label:"Feburary"},
+      {value:"March",label:"March"},
+      {value:"April",label:"April"},
+      
+
+    ];
+    const handleChange = selectedOption =>{
+      console.log(selectedOption)
+
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Select options={options} onChange={handleChange}/>
   );
 }
 
